81a870ad9ac63b91f5bae1c7a6a0226c8ee77d42
YSM-0 <yosama.production@gmail.com>
2023-03-14 16:30:58 +0100

    okay

---
+++ script.js
@@ -0,0 +1,7 @@
+const firstName = document.getElementById('first-name')
+const lastName = document.getElementById('last-name')
+const email = document.getElementById('email')
+const country = document.getElementById('country')
+const zip = document.getElementById('zip')
+const password = document.getElementById('password')
+const passwordConfirm = document.getElementById('password-confirm')
